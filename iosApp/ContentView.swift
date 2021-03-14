16a387d28c124fd68cb481b2aeeec90289753bf2
Aulia <aulia.nastiti@quipper.com>
2021-03-14 22:03:35 +0700

    init

---
+++ iosApp/ContentView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import shared
+import AVKit
+
+extension Video: Identifiable { }
+
+struct ContentView: View {
+    
+    @ObservedObject var viewModel = ViewModel(fetchVideo: FetchVideoImpl(videoRepository: VideoRepositoryImpl(videoApi: VideoApiImpl())))
+    
+    var body: some View {
+        NavigationView{
+            if(viewModel.state.isLoading){
+                MainLoadingView()
+            }else if(viewModel.state.data != nil){
+                MainVideoListView(videos: viewModel.state.data!)
+            }else{
+                MainInitialView(viewModel: self.viewModel)
+            }
+        }.onAppear(){
+            viewModel.startObserving()
+            
+            
+        }.onDisappear(){
+            viewModel.stopObserving()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
